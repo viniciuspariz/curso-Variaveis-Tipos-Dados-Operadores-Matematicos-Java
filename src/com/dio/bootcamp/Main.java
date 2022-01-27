ed14ee082672c6eee59b96c1cc12dc60bd9b9e81
viniciuspariz <viniciuspariz@gmail.com>
2022-01-27 00:19:33 -0300

    First Commit

---
+++ src/com/dio/bootcamp/Main.java
@@ -0,0 +1,56 @@
+package com.dio.bootcamp;
+
+    /*
+    *   Class criada para o exercício da Aula 1 de Variáveis, Tipos de Dados e Operadores Matemáticos
+    */
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int i;
+        //int i;
+        int I;
+        //int 1i;
+        int _1ia;
+        int $aq;
+
+        i = 5;
+        I = 10;
+        _1ia = 20;
+        $aq = 7;
+
+        final int J = 10;
+        //J = 15;
+        int asrn24678md;
+        //int asrn2467 8md;
+        int asrn2$4678_md;
+        //int asrn2$46%78_md;
+
+
+        asrn24678md = 100;
+        asrn2$4678_md = 10;
+
+        int quantidadedeProduto = 50;
+        //int QuantidadedeProduto;
+        final int NUMERO_TENTATIVAS = 5;
+        //final int numero_tentativas = 5;
+        int QUANTIDADE_OPCOES = 25;
+        //int qntdProd;
+
+        System.out.println(i);
+        System.out.println(I);
+        System.out.println(_1ia);
+        System.out.println($aq);
+
+        System.out.println(J);
+        System.out.println(asrn24678md);
+        System.out.println(asrn2$4678_md);
+
+        System.out.println(quantidadedeProduto);
+        System.out.println(NUMERO_TENTATIVAS);
+        System.out.println(QUANTIDADE_OPCOES);
+
+    }
+
+}
