ce8ce8aa2a60c07548a414278faf254b24851606
viniciuspariz <viniciuspariz@gmail.com>
2022-01-27 22:13:12 -0300

    Adicionando os exercícios da Aula 02

--- src/com/dio/bootcamp/Main.java
+++ src/com/dio/bootcamp/Main.java
@@ -1,12 +1,15 @@
 package com.dio.bootcamp;
 
-    /*
-    *   Class criada para o exercício da Aula 1 de Variáveis, Tipos de Dados e Operadores Matemáticos
-    */
+/*
+ *   Class criada para o exercício da Aula 1 de Variáveis, Tipos de Dados e Operadores Matemáticos
+ *   Aula 02 adicionada ao método main
+ */
 
 public class Main {
 
     public static void main(String[] args) {
+
+        // Exercícios da Aula 01
 
         int i;
         //int i;
@@ -26,7 +29,6 @@ public class Main {
         //int asrn2467 8md;
         int asrn2$4678_md;
         //int asrn2$46%78_md;
-
 
         asrn24678md = 100;
         asrn2$4678_md = 10;
@@ -50,6 +52,57 @@ public class Main {
         System.out.println(quantidadedeProduto);
         System.out.println(NUMERO_TENTATIVAS);
         System.out.println(QUANTIDADE_OPCOES);
+
+        // Exercício Aula 02
+
+        byte b1 = 10;
+        byte b2 = 20;
+
+        short s1 = 20000;
+        //short s2 = 40000;
+
+        //int i11 = -100000000000;
+        int i2 = 28500;
+
+        long l1 = 1000000000000000000L;
+        long l2 = 2004005000500055000L;
+
+        //float f1 = 4.5;
+        float f2 = 10.68F;
+
+        double d1 = 85.69;
+        double d2 = 99.84D;
+
+        char c1 = 'W';
+        //char c2 = 'Tw';
+        char c3 = '\u0057';
+        char c4 = '\u0013';
+
+        String st1 = "Fulano";
+        String st2 = "Cicrano";
+        String st3 = "as asd modmsod 2 2 o sd q sd,sdml ";
+
+        //String dt1 = "26/01/1987";
+
+        boolean bo1 = true;
+        boolean bo2 = false;
+
+        System.out.println(b1);
+        System.out.println(b2);
+        System.out.println(s1);
+        System.out.println(i2);
+        System.out.println(f2);
+        System.out.println(d1);
+        System.out.println(d2);
+        System.out.println(c1);
+        System.out.println(c3);
+        System.out.println(st1);
+        System.out.println(st2);
+        System.out.println(st3);
+        System.out.println(bo1);
+        System.out.println(bo2);
+        System.out.println(c4);
+        System.out.println(st1 + c4 + st2);
 
     }
 
